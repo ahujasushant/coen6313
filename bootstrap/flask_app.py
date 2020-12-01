d4f24e4c016d5a4ffd1e6ec39a9b4fc47c866405
Rkaur2007 <60591932+Rkaur2007@users.noreply.github.com>
2020-12-01 15:16:27 -0500

    bootstrap
    THis does that

---
+++ bootstrap/flask_app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+# create the object of Flask
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+
+
+# creating our routes
+@app.route('/')
+def index():
+    return render_template('index.html', content="data")
+
+
+# contact routes
+@app.route('/contact')
+def contact():
+    return render_template('contact_doctors.html')
+
+# logout routes
+@app.route('/logout')
+def contact():
+    return render_template('logout.html')
+
+
+# run flask app
+if __name__ == "__main__":
+    app.run(debug=True)
